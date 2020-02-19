b16bb83522078490cb4a5d6d6cd5211edd4a5f17
Jiadong Wu <32341494+VictorJW@users.noreply.github.com>
2020-02-18 19:25:11 -0500

    map prototype done

--- app/server.R
+++ app/server.R
@@ -7,16 +7,6 @@
 #    http://shiny.rstudio.com/
 #
 
-#library(shiny)
-#install.packages('rsconnect')
-
-#rsconnect::setAccountInfo(name='columbia-stats-slwkq',
-                         # token='804922F53FD914A6C8FB1F3F564EFFD1',
-                        #  secret='I1Ar5KuxJBynF0ETNpB/k+cmvDAiie9fMLOCrpMY')
-
-#library(rsconnect)
-#rsconnect::deployApp('../Spring2020-Project2-group-5/app/')
-
 library(shiny)
 library(leaflet)
 library(data.table)
@@ -26,13 +16,18 @@ library(googleVis)
 library(geosphere)
 library(leaflet.extras)
 library(ggmap)
+library(tidyverse)
 
-source("global.R")
+# MAP
 
-#Get the Google API
+# JW
+free_condom <- read_csv('C:\\Users\\Dr.FlyOnBeD\\Dropbox\\CU_LIFE\\Applied_Data_Science\\Spring2020-Project2-group-5\\data\\homeless\\NYC_Condom_Availability_Program_-_HIV_condom_distribution_locations.csv')
+drop_in <- read_csv('C:\\Users\\Dr.FlyOnBeD\\Dropbox\\CU_LIFE\\Applied_Data_Science\\Spring2020-Project2-group-5\\data\\homeless\\Directory_Of_Homeless_Drop-_In_Centers.csv')
+job_center <- read_csv('C:\\Users\\Dr.FlyOnBeD\\Dropbox\\CU_LIFE\\Applied_Data_Science\\Spring2020-Project2-group-5\\data\\homeless\\Directory_Of_Job_Centers.csv')
+
 register_google(key = "AIzaSyAXxi_jjBKmoortYOFU1WeenatppEgJgdc")
-shinyServer(function(input, output,session)
-{
+marker_opt <- markerOptions(opacity = 0.7, riseOnHover = TRUE)
+shinyServer(function(input, output,session){
   output$map <- renderLeaflet({
     m <- leaflet() %>%
       addProviderTiles("CartoDB.Positron", 
@@ -40,68 +35,44 @@ shinyServer(function(input, output,session)
       setView(-73.9252853,40.7910694,zoom = 13) %>%
       addResetMapButton()
     
-    #plot kids activities
-    leafletProxy("map", data = kid_activity) %>%
+    
+    leafletProxy("map", data = free_condom) %>%
       addMarkers(~Longitude, ~Latitude,
-                 group = "kid_activity" ,
-                 options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
-                                                        "<br/>", "Danger Index: ", Danger_Index,
-                                                        "<br/>", "Phone: ", Contact.Number,
-                                                        "<br/>", "Address: ", Location.1, 
-                                                        " ",Postcode) ,
-                 label = ~ SITE.NAME ,
+                 group = "free_condom" ,
+                 options = marker_opt,  
                  icon = list(iconUrl = 'https://cdn2.vectorstock.com/i/1000x1000/47/76/kids-icon-happy-boy-and-girl-children-silhouettes-vector-9674776.jpg'
                              ,iconSize = c(25,25)))
-    leafletProxy("map", data = middle_activity) %>%
-      addMarkers(~Longitude, ~Latitude ,
-                 group = "middle_activity"
-                 , options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
-                                                          "<br/>", "Danger Index: ", Danger_Index,
-                                                          "<br/>", "Phone: ", Contact.Number,
-                                                          "<br/>", "Address: ", Location.1, 
-                                                          " ",Postcode) ,  
-                 label = ~ SITE.NAME ,
+    
+    leafletProxy("map", data = drop_in) %>%
+      addMarkers(~Longitude, ~Latitude,
+                 group = "drop_in" ,
+                 options = marker_opt,  
                  icon = list(iconUrl = 'https://cdn3.iconfinder.com/data/icons/school-pack-3-1/512/5-512.png'
                              ,iconSize = c(25,25)))
-    leafletProxy("map", data = high_activity) %>%
+    
+    leafletProxy("map", data = job_center) %>%
       addMarkers(~Longitude, ~Latitude,
-                 group = "high_activity"
-                 , options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
-                                                          "<br/>", "Danger Index: ", Danger_Index,
-                                                          "<br/>", "Phone: ", Contact.Number,
-                                                          "<br/>", "Address: ", Location.1, 
-                                                          " ",Postcode) ,  
-                 label = ~ SITE.NAME,
+                 group = "job_center" ,
+                 options = marker_opt,  
                  icon = list(iconUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Closed_Book_Icon.svg/512px-Closed_Book_Icon.svg.png'
                              ,iconSize = c(25,25)))
     m
-  }
-  )
+  
+  
 })
-# MAP
 
-# JW
-
-
-
-# KW
-
-
-
-
-
-
+  observeEvent(input$enable_markers, {
+    if("Free Condom" %in% input$enable_markers) leafletProxy("map") %>% showGroup("free_condom")
+    else{leafletProxy("map") %>% hideGroup("free_condom")}
+    if("Drop-In Center" %in% input$enable_markers) leafletProxy("map") %>% showGroup("drop_in")
+    else{leafletProxy("map") %>% hideGroup("drop_in")}
+    if("Job Center" %in% input$enable_markers) leafletProxy("map") %>% showGroup("job_center")
+    else{leafletProxy("map") %>% hideGroup("job_center")}
+  }, ignoreNULL = FALSE)
+  
+})
 
 
 
-# REPORT
-
-# JB
 
 
-
-# HL
-
-
-
-# TZ
--- app/ui.r
+++ app/ui.r
@@ -13,45 +13,32 @@ library(data.table)
 library(plotly)
 library(shinythemes)
 library(shinyWidgets)
+
+# MAP
+
+# JW
 shinyUI
 (
   div
   (id = 'canvas',
-    navbarPage
-    (strong("Are Children Living in Danger?",style="color: white;"), 
-      theme=shinytheme("cerulean"),
-      tabPanel('Map',
-               div(class="outer",
-                   leafletOutput("map",width="100%",height=700),
-               )
-      )
+      
+  navbarPage
+    (strong("Homeless needs help?",style="color: white;"), 
+             theme=shinytheme("cerulean"),
+             
+             tabPanel('Map',
+                      div(class="outer",
+                      leafletOutput("map",width="100%",height=700),
+                      
+                      absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                                    top = 170, left = 20, right = "auto", bottom = "auto", width = 250, height = "auto",
+                                    
+                      checkboxGroupInput("enable_markers", "Homeless Resources:",
+                                          choices = c("Free Condom","Drop-In Center","Job Center"),
+                                          selected = c("Free Condom","Drop-In Center","Job Center"))
+                      )
+                )
+             )
     )
   )
 )
-# MAP
-
-# JW
-
-
-
-# KW
-
-
-
-
-
-
-
-
-
-# REPORT
-
-# JB
-
-
-
-# HL
-
-
-
-# TZ
